8de6cb33c221da81157413bc5d32d1ed9cf9e5ba
lucky <45010443>
2019-04-26 13:05:35 +0800

    springboot 学习

---
+++ src/main/java/com/zhaomlb/club/bootsp/BootspApplication.java
@@ -0,0 +1,15 @@
+package com.zhaomlb.club.bootsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BootspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootspApplication.class, args);
+
+	}
+
+}
